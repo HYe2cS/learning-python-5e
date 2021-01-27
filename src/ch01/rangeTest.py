813417ddfe222e362e74c4ec2b6a69bd40ea12a8
YJ2CS <cnyjzhang@outlook.com>
2021-01-27 22:02:38 +0800

    reformat

--- src/ch01/rangeTest.py
+++ src/ch01/rangeTest.py
@@ -1,7 +1,5 @@
-N = int(input("N=?"))
-result=0
-for i in range(N+1):
-    result+=i
-print("result=",result)
-
-
+N = int(input("N=?\n"))
+result = 0
+for i in range(N + 1):
+    result += i
+print("result=", result)
